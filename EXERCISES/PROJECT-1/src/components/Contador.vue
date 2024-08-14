95108a679ed35218fe1f1194dc1d7e782ad269d2
juanzch <juanmacardenas.co@gmail.com>
2024-08-13 22:05:35 -0500

    counter component creation

---
+++ EXERCISES/PROJECT-1/src/components/Contador.vue
@@ -0,0 +1,28 @@
+
+
+<template>
+<h1>Hola mundo</h1>
+<p>{{ contador }}</p>
+<button @click="incrementar">incrementar</button>
+<button @click="decrementar">decrementar</button>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const contador = ref(0);
+const incrementar = ()=>{
+  contador.value++;
+}
+const decrementar = ()=>{
+  contador.value--;
+}
+</script>
+
+<style scoped>
+p{
+  color: blueviolet;
+  font-size: 3rem;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+</style>
